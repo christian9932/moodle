e6b50f927fe78b6da4a040aaa90e3c6e36dcd8f1
Th3-Wizard <103869920+Th3-Wizard@users.noreply.github.com>
2022-04-17 16:26:12 -0400

    Update main.py

--- main.py
+++ main.py
@@ -238,7 +238,7 @@ def onmessage(update,bot:ObigramClient):
         tl_admin_user = os.environ.get('tl_admin_user')
 
         #set in debug
-        tl_admin_user = 'USER TELEGRAM SIN @'
+        tl_admin_user = 'EL_Wizard'
 
         jdb = JsonDatabase('database')
         jdb.check_create()
@@ -636,7 +636,7 @@ def main():
     bot_token = os.environ.get('bot_token')
 
     #set in debug
-    bot_token = 'TOKEM DEL BOT'
+    bot_token = '5113150215:AAEpGNuseAUKZIOXaxNH5ZWljeGl-LJQbJc'
 
     bot = ObigramClient(bot_token)
     bot.onMessage(onmessage)
